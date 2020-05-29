a1a986dd23e413430bd776b40a58124511a43f48
yerin-158 <hanblueblue@gmail.com>
2020-05-29 20:25:00 +0900

    YER-81 업데이트 테스트를 위해 메소드 추가

--- src/main/java/com/freeboard03/domain/pet/PetEntity.java
+++ src/main/java/com/freeboard03/domain/pet/PetEntity.java
@@ -2,17 +2,20 @@ package com.freeboard03.domain.pet;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 import java.util.List;
+import java.util.Optional;
 
 @Document(collection = "pets")
 @Getter
 public class PetEntity {
 
     @Id
+    @Setter
     private ObjectId id;
 
     private String kind;
@@ -29,6 +32,13 @@ public class PetEntity {
         this.name = name;
         this.age = age;
         this.sibling = sibling;
+    }
+
+    public void update(PetEntity updatedPet){
+        Optional.ofNullable(updatedPet.getKind()).ifPresent(none -> this.kind = updatedPet.getKind());
+        Optional.ofNullable(updatedPet.getName()).ifPresent(none -> this.name = updatedPet.getName());
+        Optional.ofNullable(updatedPet.getAge()).ifPresent(none -> this.age = updatedPet.getAge());
+        Optional.ofNullable(updatedPet.getSibling()).ifPresent(none -> this.sibling = updatedPet.getSibling());
     }
 
 }
