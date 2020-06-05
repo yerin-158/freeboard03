bf96613bbb3ce7db899264f1d8b12b322aefb295
yerin-158 <hanblueblue@gmail.com>
2020-06-05 20:52:53 +0900

    YER-85 [개발] 비즈니스 로직에 필요한 레포지토리 메소드를 작성한다.

--- src/main/java/com/freeboard03/domain/board/BoardOperator.java
+++ src/main/java/com/freeboard03/domain/board/BoardOperator.java
@@ -1,6 +1,8 @@
 package com.freeboard03.domain.board;
 
 import com.freeboard03.domain.board.enums.SearchType;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,11 +10,18 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Repository
@@ -47,6 +56,43 @@ public class BoardOperator {
         }
 
         return query.with(pageable);
+    }
+
+    public UpdateResult update(ObjectId id, BoardEntity updatedEntity) {
+        Query query = new Query(Criteria.where("_id").is(id));
+        Update update = getUpdate(updatedEntity);
+        return operations.updateFirst(query, update, BoardEntity.class);
+    }
+
+    private Update getUpdate(BoardEntity updatedEntity) {
+
+        try {
+            List getterMethodNames = getBoardEntityGetterMethodName();
+            Update update = new Update().currentDate("updatedAt");
+
+            for (Method method : BoardEntity.class.getDeclaredMethods()) {
+                if (getterMethodNames.contains(method.getName())) {
+                    Object obj = method.invoke(updatedEntity);
+                    Optional.ofNullable(obj).ifPresent(none -> update.set(getField(method), obj));
+                }
+            }
+
+            return update;
+
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.getStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private List getBoardEntityGetterMethodName() {
+        Field[] fields = BoardEntity.class.getDeclaredFields();
+        return Arrays.asList(fields).stream().map(field -> "get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1)).collect(Collectors.toList());
+    }
+
+    private String getField(Method method) {
+        String Field = method.getName().substring(3);
+        return Field.substring(0, 1).toLowerCase() + Field.substring(1);
     }
 
 }
